7da6cec801b5a91e8778e7f40bc4829615a98ce0
lucic71 <lucian.popescu187@gmail.com>
2021-03-26 12:03:45 +0200

    Added HttpClientInMemory and fetched data from it

--- tour-of-heroes/src/app/hero.service.ts
+++ tour-of-heroes/src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -10,12 +11,24 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-    constructor(private messageService: MessageService) { }
+    private heroesUrl = 'api/heroes';
+
+    constructor(
+        private http: HttpClient,
+        private messageService: MessageService
+    ) { }
+
+    private log(message: string) {
+        this.messageService.add(`HeroService: ${message}`);
+    }
 
     getHeroes(): Observable<Hero[]> {
-        const heroes = of(HEROES);
-        this.messageService.add('HeroService: fetched heroes');
-        return heroes;
+        return this.http.get<Hero[]>(this.heroesUrl);
+    }
+
+    getHero(id: number): Observable<Hero> {
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.get<Hero>(url);
     }
 
 }
