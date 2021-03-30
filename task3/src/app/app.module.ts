dba3805d4d42af846064099d54b80fc064ae43bc
lucic71 <lucian.popescu187@gmail.com>
2021-03-30 13:24:26 +0300

    Defined routes

--- task3/src/app/app.module.ts
+++ task3/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -14,7 +15,11 @@ import { EmployeesComponent } from './components/employees/employees.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+      HttpClientModule,
+      RouterModule.forRoot([
+          {path: 'projects', component: ProjectsComponent},
+          {path: 'employees', component: EmployeesComponent},
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
